a81eab85d18828cce093fb4ff7bf947cb7d2dcf1
André Cezario <developer.andrecezario@gmail.com>
2023-06-12 01:02:22 -0300

    definindo a conecção com o mongodb

---
+++ src/database/index.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const connect = () => {
+    mongoose.connect('mongodb://127.0.0.1:27017/')
+
+    const db = mongoose.connection
+
+    db.once('open', () => {
+        console.log('Connected to database!')
+    })
+
+    db.on('error', console.error.bind(console,"deu ruim!"))
+}
+
+module.exports = {
+    connect
+}
