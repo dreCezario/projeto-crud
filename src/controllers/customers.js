e3c1403c594937f1749bdfe6444e7b9173d99022
André Cezario <developer.andrecezario@gmail.com>
2023-06-16 19:09:30 -0300

    criando, definindo rota e estilizando a página de editar usuário

--- src/controllers/customers.js
+++ src/controllers/customers.js
@@ -33,17 +33,54 @@ const add = async (req, res) => {
     })
 }
 
-const listUsers = async (req, res) => {
+const list = async (req, res) => {
     const users = await CustomersModel.find()
 
-    res.render('listUsers', {
+    res.render('list', {
         title: 'Listagem de usuários',
         users: users,
+    })
+}
+
+const formEdit = async (req, res) => {
+    const { id } = req.query
+
+    const user = await CustomersModel.findById(id)
+
+    res.render('edit', {
+        title: 'Editar Usuário',
+        user,
+    })
+}
+
+const edit = async (req, res) => {
+    const {
+        name,
+        age,
+        email
+    } = req.body
+
+    const { id } = req.params
+
+    const user = await CustomersModel.findById(id)
+
+    user.name = name
+    user.age = age
+    user.email = email
+
+    user.save()
+
+    res.render('edit', {
+        title: 'Editar Usuário',
+        user,
+        message: 'Usuário alterado com sucesso!'
     })
 }
 
 module.exports = {
     index,
     add,
-    listUsers,
+    list,
+    formEdit,
+    edit,
 }
